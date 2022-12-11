d5e06567a48b5ce5deca0387b6158639220cc026
roger.luo <morro_luo@163.com>
2022-12-11 22:40:33 +0800

    支持AT指令解析及回显控制,解决缓冲区满之后收不到新数据的问题

--- framework/cli.c
+++ framework/cli.c
@@ -1,7 +1,7 @@
 /******************************************************************************
  * @brief    命令行处理
  *
- * Copyright (c) 2015-2020, <morro_luo@163.com>
+ * Copyright (c) 2015-2020, <master_roger@sina.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -12,29 +12,29 @@
  * 2017-07-04      Morro        优化字段分割处理
  * 
  * 2020-07-05      Morro        使用cli_obj_t对象, 支持多个命令源处理
- *
- * 2021-08-30      Morro        解决缓冲区满之后收不到新数据的问题
+ * 2020-08-29      Morro        支持AT指令解析及回显控制,解决缓冲区满之后收不到新数据的问题
+ * 2020-02-16      Morro        添加命令行守卫处理程序
  ******************************************************************************/
 #include "cli.h"
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 static const cmd_item_t cmd_tbl_start SECTION("cli.cmd.0") = {0};
 static const cmd_item_t cmd_tbl_end SECTION("cli.cmd.4") = {0};    
-/*
+/**
  * @brief       查找命令
  * @param[in]   keyword - 命令关键字
  * @return      命令项
  */ 
-static const cmd_item_t *find_cmd(const char *keyword)
+static const cmd_item_t *find_cmd(const char *keyword, int n)
 {                   
 	const cmd_item_t *it;
     for (it = &cmd_tbl_start + 1; it < &cmd_tbl_end; it++) {
-		if (!strcasecmp(keyword, it->name))
-		    return it;
+        if (!strncasecmp(keyword, it->name, n))
+            return it;
     }
 	return NULL;
 }
@@ -78,7 +78,7 @@ static size_t strsplit(char *s, const char *separator, char *list[],  size_t len
     return count;
 }
 
-/*
+/**
  *@brief 打印一个格式化字符串到串口控制台
  *@retval 
  */
@@ -90,11 +90,83 @@ static void cli_print(cli_obj_t *obj, const char *format, ...)
 	va_start (args, format);
 	len = vsnprintf (buf, sizeof(buf), format, args);
 	va_end (args);
-    obj->write(buf, len);
+    obj->write(buf, len);    
 }
 
 
-/*
+/**
+ * @brief       处理行
+ * @param[in]   line - 命令行
+ * @return      none
+ **/
+static void process_line(cli_obj_t *obj)
+{
+    char *argv[CLI_MAX_ARGS];
+    int   argc, ret, isat = 0;
+    const cmd_item_t *it;
+    
+    //命令拦截
+    if (obj->guard && !obj->guard(obj->recvbuf))
+        return;
+    
+    if (obj->echo) {  //回显
+        obj->print(obj,"%s\r\n",obj->recvbuf);
+    }
+
+    argc = strsplit(obj->recvbuf, ",",argv, CLI_MAX_ARGS);
+    
+    const char *start, *end;
+
+    if (argv[0] == NULL)
+        return;
+
+    if (strcasecmp("AT", argv[0]) == 0) {
+        obj->print(obj, "OK\r\n");
+        return;
+    } 
+#if CLI_AT_ENABLE != 0
+    isat = strncasecmp(argv[0], "AT+", 3) == 0;
+#endif
+    
+    start= !isat ? argv[0] : argv[0] + 3;
+
+    if ((end = strchr(start, '=')) != NULL) {
+        obj->type = CLI_CMD_TYPE_SET;
+
+    } else if ((end = strchr(start, '?')) != NULL) {
+        obj->type = CLI_CMD_TYPE_QUERY;
+
+    } else {
+        obj->type = CLI_CMD_TYPE_EXEC;
+        end = start + strlen(argv[0]);
+    }
+    if (start == end)
+        return;
+
+    if ((it = find_cmd(start, end - start)) == NULL) {
+        obj->print(obj, "%s\r\n", isat ? "ERROR" : "");
+        return;
+    }
+    ret = it->handler(obj, argc, argv);
+    if (isat) {
+        obj->print(obj, "%s\r\n" ,ret ? "OK":"ERROR");
+    }
+}
+
+/**
+ * @brief       获取设置值
+ */
+static int get_val(struct cli_obj *self)
+{
+    char *p;
+    p = strchr(self->recvbuf, '=');
+    if ( p == NULL)
+        return 0;
+    return atoi(p + 1);
+  
+}
+
+/**
  * @brief       cli 初始化
  * @param[in]   p - cli驱动接口
  * @return      none
@@ -105,19 +177,24 @@ void cli_init(cli_obj_t *obj, const cli_port_t *p)
     obj->write  = p->write;
     obj->print  = cli_print;
     obj->enable = true;
+    obj->get_val = get_val;
+    obj->guard   = p->cmd_guard;
 }
 
-/*
+/**
  * @brief       进入cli命令模式(cli此时自动处理用户输入的命令)
  * @param[in]   none
  * @return      none
  **/
 void cli_enable(cli_obj_t *obj)
 {
+    char a;
     obj->enable = true;
+    obj->recvcnt = 0;
+    while (obj->read(&a, 1) > 0) {}
 }
 
-/*
+/**
  * @brief       退出cli命令模式(cli此时不再处理用户输入的命令)
  * @param[in]   none
  * @return      none
@@ -127,60 +204,62 @@ void cli_disable (cli_obj_t *obj)
     obj->enable = false;
 }
 
-/*
- * @brief       处理行
- * @param[in]   line - 命令行
+/**
+ * @brief       回显控制
+ * @param[in]   echo - 回显开关控制(0/1)
  * @return      none
  **/
-static void process_line(cli_obj_t *obj)
+void cli_echo_ctrl (cli_obj_t *obj, int echo)
 {
-    char *argv[CLI_MAX_ARGS];
-    int   argc;    
-    const cmd_item_t *it;
-    argc = strsplit(obj->recvbuf, " ,",argv, CLI_MAX_ARGS);
-    if ((it = find_cmd(argv[0])) == NULL) {
-        obj->print(obj, "Unknown command '%s' - try 'help'\r\n", argv[0]);
-        return;
-    }
-    it->handler(obj, argc, argv);
+    obj->echo = echo;
 }
 
-/*
+/**
  * @brief       执行一行命令(无论cli是否运行,都会执行)
  * @param[in]   none
  * @return      none
  **/
 void cli_exec_cmd(cli_obj_t *obj, const char *cmd)
 {
-    snprintf(obj->recvbuf, CLI_MAX_CMD_LEN, "%s", cmd);
+    int len = strlen(cmd);
+    if (len >= CLI_MAX_CMD_LEN - 1)
+        return;
+    strcpy(obj->recvbuf, cmd);
     process_line(obj);
 }
 
-/*
+/**
  * @brief       命令行处理程序
  * @param[in]   none
  * @return      none
  **/
 void cli_process(cli_obj_t *obj)
 {    
-    int i;
+    char buf[32];
+    int i, readcnt;
     if (!obj->read || !obj->enable)
         return;
-    i = obj->recvcnt;
-    obj->recvcnt += obj->read(&obj->recvbuf[i], CLI_MAX_CMD_LEN - i);
-    while (i < obj->recvcnt) {
-        if (obj->recvbuf[i] == '\r' || obj->recvbuf[i] == '\n') {    /*读取1行*/
-            obj->recvbuf[i] = '\0';
-            process_line(obj);
-            obj->recvcnt = 0;
+    
+    readcnt = obj->read(buf, sizeof(buf));
+    
+    if (readcnt) {
+        for (i = 0; i < readcnt; i++) {
+            if (buf[i] == '\r' || buf[i] == '\n' || buf[i] == '\0') {
+                obj->recvbuf[obj->recvcnt] = '\0';
+                if (obj->recvcnt > 1)
+                    process_line(obj);
+                obj->recvcnt = 0;
+            } else {
+                obj->recvbuf[obj->recvcnt++] = buf[i];
+                
+                if (obj->recvcnt >= CLI_MAX_CMD_LEN) /*缓冲区满之后强制清空*/
+                    obj->recvcnt = 0;
+            }
         }
-        i++;
     }
-    if (obj->recvcnt >= CLI_MAX_CMD_LEN) /*缓冲区满之后强制清空*/
-        obj->recvcnt = 0;
 }
 
-
+#if 1
 /*******************************************************************************
 * @brief	   命令比较器
 * @param[in]   none
@@ -192,6 +271,7 @@ static int cmd_item_comparer(const void *item1,const void *item2)
     cmd_item_t *it2 = *((cmd_item_t **)item2);    
     return strcmp(it1->name, it2->name);
 }
+#endif
 
 /*
  * @brief	   帮助命令
@@ -204,16 +284,15 @@ static int do_help (struct cli_obj *s, int argc, char *argv[])
 	const cmd_item_t *cmdtbl[CLI_MAX_CMDS];
     
     if (argc == 2) {
-        if ((item_start = find_cmd(argv[1])) != NULL) 
+        if ((item_start = find_cmd(argv[1], strlen(argv[1]))) != NULL) 
         {
             s->print(s, item_start->brief);                  /*命令使用信息----*/
             s->print(s, "\r\n");   
         }
         return 0;
     }
-    for (i = 0; i < item_end - item_start && i < CLI_MAX_CMDS; i++) {
+    for (i = 0; i < item_end - item_start && i < CLI_MAX_ARGS; i++)
         cmdtbl[i] = &item_start[i];
-	}
     count = i;
     /*对命令进行排序 ---------------------------------------------------------*/
     qsort(cmdtbl, i, sizeof(cmd_item_t*), cmd_item_comparer);    		        
@@ -237,4 +316,3 @@ static int do_help (struct cli_obj *s, int argc, char *argv[])
  /*注册帮助命令 ---------------------------------------------------------------*/
 cmd_register("help", do_help, "list all command.");   
 cmd_register("?",    do_help, "alias for 'help'");
-
--- framework/cli.h
+++ framework/cli.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  * @brief    命令行处理
  *
- * Copyright (c) 2015-2020, <morro_luo@163.com>
+ * Copyright (c) 2015-2020, <master_roger@sina.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -12,24 +12,41 @@
  * 2017-07-04      Morro        优化字段分割处理
  * 
  * 2020-07-05      Morro        使用cli_obj_t对象, 支持多个命令源处理
- *
- * 2021-08-30      Morro        解决缓冲区满之后收不到新数据的问题
+ * 2020-08-29      Morro        支持AT指令解析及回显控制,解决缓冲区满之后收不到新数据的问题
+ * 2020-02-16      Morro        添加命令行守卫处理程序
  ******************************************************************************/
 #ifndef _CMDLINE_H_
 #define _CMDLINE_H_
 
 #include "comdef.h"
 
-#define CLI_MAX_CMD_LEN           64             /*命令行长度*/                 
-#define CLI_MAX_ARGS              16             /*最大参数个数*/
-#define CLI_MAX_CMDS              32             /*最大允许定义的命令个数*/
+#define CLI_MAX_CMD_LEN           256            /*命令行长度*/                 
+#define CLI_MAX_ARGS              64             /*最大参数个数*/
+#define CLI_MAX_CMDS              64             /*最大允许定义的命令个数*/
+
+/** 
+ * @brief     CLI作为AT解析器使用,在接收匹配时自动过滤"AT+",
+ */
+#define CLI_AT_ENABLE             1              
+
+/*命令类型 */
+#define CLI_CMD_TYPE_EXEC         0              /* 普通执行命令*/
+#define CLI_CMD_TYPE_QUERY        1              /* 查询命令 (XXX?)*/
+#define CLI_CMD_TYPE_SET          2              /* 设备命令 (XXX=YY)*/
 
 struct cli_obj;
 
 /*命令项定义*/
 typedef struct {
-	char	   *name;		                         /*命令名*/
-    /*命令处理程序*/   
+	char	   *name;		                         /*命令名*/ 
+    /**
+     * @brief     命令处理程序,类型
+     * @params    o      - cli 对象
+     * @params    argc   - 命令参数个数
+     * @params    argv   - 命令参数表
+     * @return    命令执行结果, 对于AT指令, 返回true时会自动响应OK,返回false时则
+     *            响应ERROR
+     */       
 	int        (*handler)(struct cli_obj *o, int argc, char *argv[]);   
     const char *brief;                               /*命令简介*/
 }cmd_item_t;
@@ -50,18 +67,35 @@ typedef struct {
 
 /*cli 接口定义 -------------------------------------------------------------*/
 typedef struct {
+    /**
+     * @brief 通信数据(串口)写接口
+     */        
     unsigned int (*write)(const void *buf, unsigned int len);
+    /**
+     * @brief 通信数据(串口)读接口
+     */        
     unsigned int (*read) (void *buf, unsigned int len);
+    
+    /**
+     * @brief 命令行守卫程序(不需要则填写NULL,允许所有命令执行)
+     * @retval true - 允许执行, false - 忽略此命令
+     */         
+    int (*cmd_guard)(char *cmdline);
+    
 }cli_port_t;
 
 /*命令行对象*/
 typedef struct cli_obj {
+    int          (*guard)(char *cmdline);
     unsigned int (*write)(const void *buf, unsigned int len);
     unsigned int (*read) (void *buf, unsigned int len);
     void         (*print)(struct cli_obj *this, const char *fmt, ...); 
-    char           recvbuf[CLI_MAX_CMD_LEN + 1];  /*命令接收缓冲区*/
-    unsigned short recvcnt;                       /*最大接收长度*/    
-    unsigned       enable : 1;
+    int          (*get_val)(struct cli_obj *this);
+    char           recvbuf[CLI_MAX_CMD_LEN + 1];  /* 命令接收缓冲区*/
+    unsigned short recvcnt;                       /* 最大接收长度*/    
+    unsigned       type   : 3;                    /* 命令类型*/
+    unsigned       enable : 1;                    /* CLI 开关控制*/ 
+    unsigned       echo   : 1;                    /* 回显设置*/    
 }cli_obj_t;
 
 void cli_init(cli_obj_t *obj, const cli_port_t *p);
@@ -70,10 +104,11 @@ void cli_enable(cli_obj_t *obj);
 
 void cli_disable (cli_obj_t *obj);
 
+void cli_echo_ctrl (cli_obj_t *obj, int echo);
+
 void cli_exec_cmd(cli_obj_t *obj, const char *cmd);
 
 void cli_process(cli_obj_t *obj);
-
 
 
 #endif	/* __CMDLINE_H */
